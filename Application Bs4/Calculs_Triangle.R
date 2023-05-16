b7e48b93eb1749777b052a35b2b29dd9ae15124a
DIAKITE Abdoul Oudouss <Diakson1441@gmail.com>
2023-05-16 14:45:13 +0100

    last update

--- Application Bs4/Calculs_Triangle.R
+++ Application Bs4/Calculs_Triangle.R
@@ -3,7 +3,6 @@ TriangleRgleAT <- read_excel("Data/TriangleRgle.xlsx")
 names(TriangleRgleAT)[1] <- "Annee"
 TriangleRgleAT = TriangleRgleAT[,-1]
 TriangleRgleAT = as.matrix(TriangleRgleAT)
-cumul <- t(apply(TriangleRgleAT, 1, cumsum))
 
 # Calcul des facteurs de développement individuels ----
 
@@ -19,7 +18,7 @@ calculer_fdi <- function(triangle){
   # Initialiser la matrice de sortie
   fdi <- matrix(NA, nrow = nrow(triangle), ncol = ncol(triangle)-1)
   # Calculer les montants cumulés des sinistres réglés par année de développement
-  cumul = t(apply(triangle, 1, cumsum))
+  cumul = triangle
   # Calculer les facteurs de développement individuels par année de développement
   for (i in 1:nrow(cumul)) {
     fdi[i, ] <- cumul[i, -1] / cumul[i, -ncol(cumul)]
@@ -38,18 +37,18 @@ calculer_fdc <- function(triangle){
   # Initialiser le vecteur de sortie
   fdc <- c()
   # Calculer les montants cumulés des sinistres réglés par année de développement
-  cumul = t(apply(triangle, 1, cumsum))
+  cumul = triangle
   n = nrow(cumul)
   # Calculer les facteurs de développement individuels par année de développement
   for (j in 1:(n-1)) {
     numerateur = sum(cumul[1:(n-j),j+1])
-    denominateur = sum(cumul[1:(n-j+1),j])
+    denominateur = sum(cumul[1:(n-j),j])
     fdc[j] = numerateur/denominateur
   }
   # Renvoyer la matrice des facteurs de développement individuels
   return(fdc)
 }
-
+fdc = calculer_fdc(triangle)
 #  Les règlements cumulés futurs estimés
 C_hat <- function(triangle,fdc){
   # Vérifier que le triangle est une matrice carrée
@@ -58,20 +57,66 @@ C_hat <- function(triangle,fdc){
   }
   # Initialiser la matrice de sortie
   n = nrow(triangle)
-  c_hat <- matrix(nrow = n,ncol = n)
   # Calculer les montants cumulés des sinistres réglés par année de développement
-  cumul = t(apply(triangle, 1, cumsum))
-  fdc = c(1,fdc)
-  for (i in 1:n) {
-
-      c_hat[i,(n-i+1):n] = cumul[i,n-i+1]*cumprod(fdc[(n-i+1):n])
-
-
-  }
-  c_hat
+  cumul = triangle
+  fdc = c(fdc,1)
+  # Construction du prod des fdc
+  resultat = Map(\(x) cumprod(fdc[x:n]), x =n:1)
+  resultat = Map(\(x) append(x[-length(x)],NA,0), x= resultat)
+  # On prend les elements de la diagonale secondaire
+  Diag = diag(apply(cumul,1,rev))
+  # On applique la formule
+  resultat = Map(\(x,y) x*y, x =resultat, y = Diag)
+  resultat = Map(\(x,y) c(rep(NA,x-1),y), x = n:1, y = resultat)
+  resultat = matrix(unlist(resultat), nrow = n, byrow = TRUE)
+  resultat
 
 }
-c_hat <- C_hat(triangle = triangle,fdc = fdc)
 
+Diag = diag(apply(triangle,1,rev))
+c_hat <- C_hat(triangle = triangle,fdc = fdc)
+PSAP = sum(c_hat[,n] - Diag,na.rm = TRUE)
+
+
+
+# Constitution du triangle des règlements décumulés futurs par année de survenance :
+
+# Triangle decumulé
+decumul_tri <- function(triangle, c_hat){
+  n = nrow(triangle)
+  cumul = triangle
+  triangleGLobal = matrix(nrow = n, ncol = n)
+  decumul = matrix(nrow = n, ncol = n)
+  for (i in 1:n) {
+    for (j in 1:n) {
+      triangleGLobal[i,j] = sum(c(cumul[i,j], c_hat[i,j]),na.rm = TRUE)
+    }
+  }
+
+  decumul = triangleGLobal
+  for (i in 1:n) {
+    decumul[i,2:n] = triangleGLobal[i,2:n] - triangleGLobal[i,1:(n-1)]
+  }
+  decumul
+}
+
+decumul = decumul_tri(triangle,c_hat)
+
+R_hat <- function(decumul){
+  # On renverse la matrice decumuler pour faciliter l'obtention des diags secondaires
+  rev_decumul =  apply(decumul,1,rev)
+  n = nrow(decumul)
+  r_hat = c()
+  for (i in 1:(n-1)) {
+    r_hat[i] = sum(diag(rev_decumul[1:(n-i+1),i:n]))
+  }
+  r_hat[n] = rev_decumul[1,n]
+  r_hat[-1]
+}
+
+r_hat = R_hat(decumul)
+
+# La meilleure estimation des engagements pour sinistres nets -----
+BE_Sinistre = sum(r_hat/((1+TZC$`Taux zéro coupon`[1:length(r_hat)])^(1:length(r_hat))))
 
 
