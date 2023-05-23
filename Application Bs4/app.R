02b0631d35c56d3503d78a4c55a655ce31fa9cb8
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2023-05-23 17:26:04 +0000

    application

--- Application Bs4/app.R
+++ Application Bs4/app.R
@@ -24,8 +24,9 @@ ui <- dashboardPage(
     br(),
     sidebarMenu(
       menuItem(text = "Paramètres", tabName = "parametres", icon = icon("sliders")),
-      menuItem(text = "Courbe des taux", tabName = "courbe_taux", icon = icon("chart-line"))
-    ),
+      menuItem(text = "Courbe des taux", tabName = "courbe_taux", icon = icon("chart-line")),
+      menuItem(text = "Non-vie hors rentes", tabName = "nonvieHR", icon = icon("car-burst"))
+    )
   ),
   # Body content----------
   dashboardBody(
@@ -102,6 +103,168 @@ ui <- dashboardPage(
             )
           )
         )
+      ),
+      tabItem(
+        tabName = "nonvieHR",
+          uiOutput("valueBoxs")
+        ,
+        fluidRow(
+          box(
+            status = "navy", solidHeader = T, width = 6,
+            title = "Importation du triangle de réglement",
+            # Input: Select a file
+            fileInput("fileTriangle", "Choisir le triangle de réglements",
+              buttonLabel = "Parcourir...",
+              multiple = FALSE,
+              accept = c(
+                "text/csv",
+                "text/comma-separated-values,text/plain",
+                ".csv",
+                ".xls",
+                ".xlsx"
+              )
+            ),
+            # Horizontal line
+            tags$hr(),
+            fluidRow(
+              column(
+                12,
+                # Input: Checkbox if file has header
+                checkboxInput("cumuler", "Voulez vous cumuler le triangle ?", FALSE)
+              )
+            ),
+            # Horizontal line
+            tags$hr(),
+            fluidRow(
+              column(
+                4,
+                # Input: Select separator
+                radioButtons("sep", "Separator",
+                             choices = c(
+                               Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"
+                             ),
+                             selected = ","
+                )
+              ),
+              column(
+                5,
+                offset = 1,
+                # Input: Select quotes
+                radioButtons("quote", "Quote",
+                             choices = c(
+                               None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"
+                             ),
+                             selected = '"'
+                )
+              )
+            ),
+            column(
+              12,
+                actionButton(inputId = "tgl_btn", "Confirmer la lecture du triangle" , status = "success", width = "100%")
+            )
+
+
+          ),
+          box(
+            status = "navy", solidHeader = T, width = 6,
+            title = "Paramètrages",
+            fluidRow(
+              column(
+                12,
+                tags$h5(tags$b("Primes Acquises (PA)")),
+                tags$hr()
+              ),
+              column(
+                4,
+                numericInput("pa0", "Année n",value = NULL)
+              ),
+              column(
+                4,
+                numericInput("pa1", "Année n-1",value = NULL)
+              ),
+              column(
+                4,
+                numericInput("pa2", "Année n-2",value = NULL)
+              )
+            ),
+            tags$hr(),
+            fluidRow(
+              column(
+                4,
+                numericInput("ppna","PPNA",value = NULL)
+              ),
+              column(
+                4,
+                numericInput("pfp","Primes futurs",value = NULL)
+              ),
+              column(
+                4,
+                numericInput("taux_acq","Taux frais d’acquisition",value = NULL)
+              )
+            ),
+            fluidRow(
+              column(
+                9,
+                numericInput("tx_FG_Moyen", label = "Taux de frais gestion moyens", value = NULL, min = 0,max = 1)
+              ),
+              column(
+                3,
+                actionButton(inputId = "param_btn", "Confirmer", style = "margin-top:30px", status = "success", width = "100%")
+              )
+            )
+          )
+
+        ),
+        fluidRow(
+          tabBox(
+             solidHeader = T, width = 12,maximizable = TRUE,
+            type = "tabs", side = "right",status = "navy",
+            tabPanel(
+              tags$b("Apperçu du triangle"),
+              # Triangle de réglements
+              DT::dataTableOutput("trgl_data", width = "100%")
+            ),
+            tabPanel(
+              tags$b("Calcul des règlements cumulés futurs"),
+              # Triangle de reglements
+              DT::dataTableOutput("trgl_global", width = "100%")
+            )
+
+            )
+        ),
+        fluidRow(
+          tabBox(
+            solidHeader = T, width = 6,
+            type = "tabs", side = "right",status = "navy",
+            title = "Cadence de liquidations",
+            tabPanel(
+              tags$b("Graphique"),
+              plotlyOutput("cad_plot")
+            ),
+            tabPanel(
+              tags$b("Table"),
+              DT::dataTableOutput("cad_data")
+            )
+          ),
+          tabBox(
+            solidHeader = T, width = 6,
+            type = "tabs", side = "right",status = "navy",
+            title = "Cash flows",
+            tabPanel(
+              tags$b("Graphique"),
+              plotlyOutput("cf_plot")
+            ),
+            tabPanel(
+              tags$b("Table"),
+              DT::dataTableOutput("cf_data")
+            )
+          )
+
+        )
       )
     )
   )
@@ -110,6 +273,279 @@ ui <- dashboardPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   useAutoColor()
+  # Parametrage ------
+  parametrage <- reactive(
+    {
+      list(
+        Sum_PA = sum(c(input$pa0,input$pa1,input$pa2),na.rm = TRUE),
+        PPNA = input$ppna,
+        Prime_Futur = input$pfp,
+        tx_acq = input$taux_acq,
+        tx_FG_Moy = input$tx_FG_Moyen
+      )
+    }
+  ) %>%
+    bindEvent(input$param_btn)
+  # Ratio de sinistralité
+  RS <- reactive({
+    decumul = decumul_tri(triangleAT(),c_hat())
+    CU = apply(decumul,1,sum)
+    nn = length(CU)
+    parametrage()$Sum_PA/sum(CU[(nn-3):nn])
+  })
+  # BE sinistre
+  BE_Sin_nv <- reactive({
+    BE_Sinistre_nv(r_hat = Cash_flows(),ZC = TZC()[["Taux zéro coupon"]])
+  })
+  # BE prime
+  BE_Prime <- reactive({
+    BE_Prime_nv(frfp_hat = FRFP_hat(cad_liq = cadence(),
+                                    RS = RS(),PPNA = parametrage()[["PPNA"]],
+                                    PFP = parametrage()[["Prime_Futur"]]),
+                ZC = TZC()[["Taux zéro coupon"]],
+                PFPA = parametrage()[["Prime_Futur"]]*parametrage()[["tx_acq"]]
+                )
+  })
+  PSAP = reactive({
+    Diag = diag(apply(triangleAT(),1,rev))
+    c_hat <- C_hat(triangle = triangleAT(),fdc = calculer_fdc(triangleAT()))
+    psap =  sum(c_hat[,ncol(c_hat)] - Diag,na.rm = TRUE)
+    psap
+  })
+  output$valueBoxs <- renderUI(
+    fluidRow(
+      column(
+        4,
+        valueBox(
+          value =  tags$h3(round(BE_Sin_nv(),3)),
+          subtitle  = tags$h5("BE Sinistre"),
+          icon = icon("car-burst"),
+          width = 12,color = "lightblue",gradient = TRUE,
+          footer = div("Meilleure estimation des engagements pour sinistres")
+        )
+      ),
+      column(
+        4,
+        valueBox(
+          value =  tags$h3(round(BE_Prime(),3)),
+          subtitle  = tags$h5("BE Prime"),
+          icon = icon("sack-dollar"),
+          width = 12,color = "lightblue",gradient = TRUE,
+          footer = div("Meilleure estimation des engagements pour prime")
+        )
+      ),
+      column(
+        4,
+        valueBox(
+          value =  tags$h3(round(PSAP(),3)),
+          subtitle  = tags$h5("PSAP"),
+          icon = icon("hand-holding-dollar"),
+          width = 12,color = "lightblue",gradient = TRUE,
+          footer = div("Provision pour sinistre à payer")
+        )
+      )
+
+
+    )
+  )
+
+  # Triangle de réglement ----
+  triangleAT <- reactive({
+    tryCatch(
+      {req(input$fileTriangle)
+        inFile <- input$fileTriangle
+        if (tools::file_ext(inFile$name) == "csv") {
+          dat <- read.csv(inFile$datapath,sep =  input$sep, input$quote)
+        } else {
+          dat <- read_excel(inFile$datapath,col_names = TRUE)
+        }
+        colnames(dat) = c("Année",0:(nrow(dat)-1))
+        annee = dat[["Année"]]
+        dat = dat[,-1]
+        dat = as.matrix(dat)
+        if(input$cumuler ) dat = t(apply(dat,1,cumsum))
+        dat = Triangulariser(dat)
+        dat = as.data.frame(dat)
+        dat[["Année"]] = annee
+        dat = dplyr::relocate(dat,"Année")
+        dat = as.data.frame(dat)
+        rownames(dat) = annee
+        dat = dat[,-1]
+        colnames(dat) = 0:(nrow(dat)-1)
+        as.matrix(dat)
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+  }) %>%
+    bindEvent(input$tgl_btn)
+  observeEvent(input$tgl_btn,
+               shinyalert::shinyalert("OK !", "Lecture du triangle confirmée avec succès" ,"success")
+               )
+
+
+  output$trgl_data <- DT::renderDataTable(
+    tryCatch(
+      {req(input$fileTriangle)
+        inFile <- input$fileTriangle
+        if (tools::file_ext(inFile$name) == "csv") {
+          dat <- read.csv(inFile$datapath,sep =  input$sep, input$quote)
+        } else {
+          dat <- read_excel(inFile$datapath,col_names = TRUE)
+        }
+        colnames(dat) = c("Année",0:(nrow(dat)-1))
+        annee = dat[["Année"]]
+        dat = dat[,-1]
+        dat = as.matrix(dat)
+        if(input$cumuler ) dat = t(apply(dat,1,cumsum))
+        dat = Triangulariser(dat)
+        dat = as.data.frame(dat)
+        dat[["Année"]] = annee
+        dat = dplyr::relocate(dat,"Année")
+        dat = as.data.frame(dat)
+        rownames(dat) = annee
+        dat = dat[,-1]
+        colnames(dat) = 0:(nrow(dat)-1)
+        dat
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    ),
+    extensions = "Buttons",
+    options = list(
+      searching = FALSE,
+      dom = "Bfrtip",
+      buttons = c("copy", "csv", "excel", "pdf", "print"),
+      paging = FALSE, ## paginate the output
+      pageLength = 100,
+      scrollX = TRUE,
+      scrollY = 500
+    )
+
+  )
+  # Calcul des règlements cumulés futurs par année de survenance
+  c_hat <- reactive(
+    {
+      fdc = calculer_fdc(triangleAT())
+      C_hat(triangleAT(),fdc)
+    }
+  )
+  # Cadence de liquidqtion
+  cadence = reactive({
+    decumul = decumul_tri(triangleAT(),c_hat())
+    CU = apply(decumul,1,sum)
+    rglts = apply(decumul,2,sum)
+    cad_liq = rglts/sum(CU)
+    cad_liq
+  })
+  # Cash flows
+  Cash_flows <- reactive(
+    {
+      decumul = decumul = decumul_tri(triangleAT(),c_hat())
+      r_hat = R_hat(decumul)
+      r_hat
+    }
+  )
+  output$trgl_global <- DT::renderDT(
+    {
+      tglinf = c_hat()
+      rownames(tglinf) = rownames(triangleAT())
+      colnames(tglinf) = colnames(triangleAT())
+      tglinf
+
+    },
+    extensions = "Buttons",
+    options = list(
+      searching = FALSE,
+      dom = "Bfrtip",
+      buttons = c("copy", "csv", "excel", "pdf", "print"),
+      paging = FALSE, ## paginate the output
+      pageLength = 100,
+      scrollX = TRUE,
+      scrollY = 500
+    )
+  )
+  output$cad_plot <- renderPlotly(
+    {
+      cad_liq = cadence()
+      cad_liq = data.frame("Cadence de liquidation" = cadence(),check.names = FALSE)
+      cad_liq$`Cadence de liquidation (%)` = paste0(gsub(",","\\.",round(100*cadence(),2)),"%")
+      plot_ly(
+        data = cad_liq, y= ~`Cadence de liquidation`,
+        type = "bar", mode = "markers",
+        hovertext = glue("Année de développement : {0:(nrow(cad_liq)-1)}
+                         Cadence de liquidation (%) : {cad_liq$`Cadence de liquidation (%)`}") ,
+        hoverinfo = 'text'
+      ) %>%
+        layout(
+          title = "Cadence de liquidation",
+          xaxis = list(title = "Année de développement",showline= T, linewidth=2, linecolor='black'),
+          yaxis = list(title = "")
+        )
+      }
+  )
+  output$cad_data <- DT::renderDT(
+    {
+      cad_liq = cadence()
+      cad_liq = data.frame("Cadence de liquidation" = cadence(),check.names = FALSE)
+      cad_liq$`Cadence de liquidation (%)` = paste0(gsub(",","\\.",round(100*cadence(),2)),"%")
+      cad_liq
+    },
+    extensions = "Buttons",
+    options = list(
+      searching = FALSE,
+      dom = "Bfrtip",
+      buttons = c("copy", "csv", "excel", "pdf", "print"),
+      paging = FALSE, ## paginate the output
+      pageLength = 100,
+      scrollX = TRUE,
+      scrollY = 500
+    )
+  )
+  output$cf_plot <- renderPlotly(
+    {
+      r_hat = data.frame("Flux de règlements futurs nets de recours" = Cash_flows(),check.names = FALSE)
+      nn = nrow(triangleAT())
+
+      rownames(r_hat) = as.numeric(rownames(triangleAT())[nn]) + 1:(nn-1)
+      r_hat[["Année"]] = rownames(r_hat)
+      plot_ly(data = r_hat,
+              x =~`Année`,y =~`Flux de règlements futurs nets de recours`,
+              type = 'bar'
+              ) %>%
+        layout(
+          title = "Flux de règlements futurs nets de recours",
+          legend = list(orientation = "h", xanchor = "center", x = 0.5),
+          xaxis = list(title = "",showline= T, linewidth=2, linecolor='black'),
+          yaxis = list(title = "",showline= T, linewidth=2, linecolor='black')
+        )
+
+    }
+  )
+  output$cf_data <- DT::renderDT(
+    {
+      r_hat = data.frame("Flux de règlements futurs nets de recours " = Cash_flows(),check.names = FALSE)
+      nn = nrow(triangleAT())
+
+      rownames(r_hat) = as.numeric(rownames(triangleAT())[nn]) + 1:(nn-1)
+      r_hat
+    },
+    extensions = "Buttons",
+    options = list(
+      searching = FALSE,
+      dom = "Bfrtip",
+      buttons = c("copy", "csv", "excel", "pdf", "print"),
+      paging = FALSE, ## paginate the output
+      pageLength = 100,
+      scrollX = TRUE,
+      scrollY = 500
+    )
+  )
+
   # Donnees a utiliser---
   # Taux de reference des bons de tresors
   TRBT <- reactive(
--- Application Bs4/functions.R
+++ Application Bs4/functions.R
@@ -1,4 +1,7 @@
 library(tidyverse)
+library(bslib)
+library(readxl)
+library(bsicons)
 library(glue)
 library(rvest)
 library(scales)
@@ -103,5 +106,139 @@ boostrapping <- function(T_INTER){
     `Taux zéro coupon (%)` = gsub("\\.",",",paste0(round(taux_ZC*100,3),"%"))
   )
 }
+
+# Triangle superieur -----
+Triangulariser <- function(TriangleRgleAT){
+  triangle0 = matrix(nrow = nrow(TriangleRgleAT), ncol = ncol(TriangleRgleAT))
+  n = nrow(TriangleRgleAT)
+  for (i in 1:n) {
+    for (j in 1:(n-i+1)) {
+      triangle0[i,j] = TriangleRgleAT[i,j]
+    }
+
+  }
+  triangle0
+}
+
+# Définir une fonction qui calcule les facteurs de développement individuels à partir d'un triangle de règlement
+# Le triangle de règlement doit être une matrice carrée avec les années de survenance en lignes et les années de développement en colonnes
+# La fonction renvoie une matrice de même dimension que le triangle de règlement, avec les facteurs de développement individuels en diagonale et des NA ailleurs
+
+calculer_fdi <- function(triangle){
+  # Vérifier que le triangle est une matrice carrée
+  if (!is.matrix(triangle) || nrow(triangle) != ncol(triangle)) {
+    stop("Le triangle doit être une matrice carrée")
+  }
+  # Initialiser la matrice de sortie
+  fdi <- matrix(NA, nrow = nrow(triangle), ncol = ncol(triangle)-1)
+  # Calculer les montants cumulés des sinistres réglés par année de développement
+  cumul = triangle
+  # Calculer les facteurs de développement individuels par année de développement
+  for (i in 1:nrow(cumul)) {
+    fdi[i, ] <- cumul[i, -1] / cumul[i, -ncol(cumul)]
+  }
+  # Renvoyer la matrice des facteurs de développement individuels
+  return(fdi)
+}
+
+# Calcul des facteurs de développement communs ------
+
+calculer_fdc <- function(triangle){
+  # Vérifier que le triangle est une matrice carrée
+  if (!is.matrix(triangle) || nrow(triangle) != ncol(triangle)) {
+    stop("Le triangle doit être une matrice carrée")
+  }
+  # Initialiser le vecteur de sortie
+  fdc <- c()
+  # Calculer les montants cumulés des sinistres réglés par année de développement
+  cumul = triangle
+  n = nrow(cumul)
+  # Calculer les facteurs de développement individuels par année de développement
+  for (j in 1:(n-1)) {
+    numerateur = sum(cumul[1:(n-j),j+1])
+    denominateur = sum(cumul[1:(n-j),j])
+    fdc[j] = numerateur/denominateur
+  }
+  # Renvoyer la matrice des facteurs de développement individuels
+  return(fdc)
+}
+#  Les règlements cumulés futurs estimés
+C_hat <- function(triangle,fdc){
+  # Vérifier que le triangle est une matrice carrée
+  if (!is.matrix(triangle) || nrow(triangle) != ncol(triangle)) {
+    stop("Le triangle doit être une matrice carrée")
+  }
+  n = nrow(triangle)
+  # Initialiser la matrice de sortie
+  n = nrow(triangle)
+  # Calculer les montants cumulés des sinistres réglés par année de développement
+  cumul = triangle
+  fdc = c(fdc,1)
+  # Construction du prod des fdc
+  resultat = Map(\(x) cumprod(fdc[x:n]), x =n:1)
+  resultat = Map(\(x) append(x[-length(x)],NA,0), x= resultat)
+  # On prend les elements de la diagonale secondaire
+  Diag = diag(apply(cumul,1,rev))
+  # On applique la formule
+  resultat = Map(\(x,y) x*y, x =resultat, y = Diag)
+  resultat = Map(\(x,y) c(rep(NA,x-1),y), x = n:1, y = resultat)
+  resultat = matrix(unlist(resultat), nrow = n, byrow = TRUE)
+  resultat
+
+}
+
+# Triangle decumulé
+decumul_tri <- function(triangle, c_hat){
+  n = nrow(triangle)
+  cumul = triangle
+  triangleGLobal = matrix(nrow = n, ncol = n)
+  decumul = matrix(nrow = n, ncol = n)
+  for (i in 1:n) {
+    for (j in 1:n) {
+      triangleGLobal[i,j] = sum(c(cumul[i,j], c_hat[i,j]),na.rm = TRUE)
+    }
+  }
+
+  decumul = triangleGLobal
+  for (i in 1:n) {
+    decumul[i,2:n] = triangleGLobal[i,2:n] - triangleGLobal[i,1:(n-1)]
+  }
+  decumul
+}
+
+# Cash flows
+R_hat <- function(decumul){
+  # On renverse la matrice decumuler pour faciliter l'obtention des diags secondaires
+  rev_decumul =  apply(decumul,1,rev)
+  n = nrow(decumul)
+  r_hat = c()
+  for (i in 1:(n-1)) {
+    r_hat[i] = sum(diag(rev_decumul[1:(n-i+1),i:n]))
+  }
+  r_hat[n] = rev_decumul[1,n]
+  r_hat[-1]
+}
+# BE Prime Non Vie-hors rente -----
+#'La somme actualisée des flux de règlements futurs probabilisés nets
+#' de recours relatifs aux sinistres non encore survenus
+
+FRFP_hat <- function(cad_liq,RS,PPNA,PFP){ cad_liq*RS*(PPNA+PFP) }
+
+BE_Prime_nv <- function(frfp_hat,ZC,PFPA){
+  sum(frfp_hat/((1+ZC[1:length(frfp_hat)])^(1:length(frfp_hat)))) - PFPA
+}
+
+# La meilleure estimation des engagements pour sinistres nets -----
+BE_Sinistre_nv = function(r_hat, ZC = TZC_31_12_2021$`Taux zéro coupon`){
+  sum(r_hat/((1+ZC[1:length(r_hat)])^(1:length(r_hat))))
+}
+
+
+
+
+
+
+
+
 
 
