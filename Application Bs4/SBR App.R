6560fcfcd93476057f7d07fcc24301d1b67e8bf8
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2023-05-25 15:14:17 +0000

    updated

---
+++ Application Bs4/SBR App.R
@@ -0,0 +1,11 @@
+#' Please do not rename this file !
+library(shiny.exe)
+hostWin(
+    appDir = 'C:/Users/ARM-Ismail/Desktop/Risk-based-solvency/Application Bs4',
+    port = getOption('shiny.port'),
+    launch.browser = TRUE,
+    host = '127.0.0.1',
+                  workerId = '',
+                  quiet = FALSE,
+                  display.mode = c('auto', 'normal', 'showcase'),
+                  test.mode = getOption('shiny.testmode', FALSE))
