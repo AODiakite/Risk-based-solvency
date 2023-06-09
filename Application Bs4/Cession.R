bc0a637c788af6866b0f8e147ae2f4f232aaaa45
DIAKITE Abdoul Oudouss <diakson1441@gmail.com>
2023-06-09 13:11:40 +0000

    Update Cession.R

--- Application Bs4/Cession.R
+++ Application Bs4/Cession.R
@@ -8,10 +8,18 @@
 #' @param taux_cession
 #' @param PD Proba de Defaut
 #'
+BEGPi = proj_cap_vect
+Ajustement_DC <- \(BEGPi,ZC,taux_cession,PD){
+  BEGPi_actualised = BEGPi/((1+ZC[1:length(BEGPi)])^(1:length(BEGPi)))
+  BEGP_cede = cumsum(rev(BEGPi_actualised))
+  BEGP_cede = rev(BEGP_cede)
+  BEGP_cede = BEGP_cede*taux_cession
+  Adj_i = Map(\(x,y){
+    0.5*max(x,0)*PD*(1-PD)^(y-1)
+  }, x = as.list(BEGP_cede), y = as.list(1:length(BEGP_cede)))
+  Adj_i = unlist(Adj_i)
+  Adj_act_i = Adj_i/((1+ZC[1:length(Adj_i)])^(1:length(Adj_i)))
 
-Ajustement_DC <- \(BEGP,ZC,taux_cession,PD){
-  BEGP_cede = BEGP*taux_cession
-  Adj_i = Map(\(x){
-    0.5*max(x,0)
-  })
+  sum(Adj_act_i)
 }
+
